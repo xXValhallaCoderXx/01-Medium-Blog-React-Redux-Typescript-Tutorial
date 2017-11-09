9a37be6c1da56459a5bfea972d3c1361fe995c66
Renate Gouveia <renate@horangi.com>
2017-11-10 02:53:54 +0800

    Added comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
+    // Entr point of our application
     entry: "./src/index.tsx",
+    // Output directory of out built files 
     output: {
         filename: "bundle.js",
         path: path.join(__dirname, "/dist"),
     },
+    // Plugin to help render bind our code to the index.html file
     plugins: [
         new HtmlWebpackPlugin({
             title: 'React Redux Typescript Example',
             template: path.join(__dirname, 'src/index.html'),
         }),
     ],
+    // Tell Webpack which JS files to load
     resolve: {
         extensions: [".ts", ".tsx", ".js"]
     },
+    // Setup our Webpack Loader
+    // TS files will pass through ts-loader and then go through Babel
     module: {
         rules: [
             {
@@ -34,7 +40,7 @@ module.exports = {
                     }
                 ]
             },
-            
+            // CSS Loader for styles
             {
                 test: /\.css$/,
                 exclude: path.join(__dirname, './node_modules'),
@@ -42,8 +48,11 @@ module.exports = {
             },
         ]
     },
+    // Create sourcemaps for debugging
     devtool: "eval-source-map",
+    // Setup our dev server
     devServer: {
+        inline: true,
         host: process.env.HOST,
         port: process.env.PORT,
     },
