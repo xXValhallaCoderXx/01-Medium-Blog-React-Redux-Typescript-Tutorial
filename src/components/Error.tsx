31dfb09e608b35f99e1410d7f48153a676916f7f
Renate Gouveia <renate@horangi.com>
2017-11-18 19:08:42 +0800

    Removed counter

--- src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ErrorMessage = (): JSX.Element => (
-    <div>
-        <p>One can not drink, if thy have no Ale!</p>
-    </div>
-);
-
-export default ErrorMessage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CounterContainer from './containers/CounterContainer';
 import AddItemContainer from './containers/AddItemContainer';
 import './assets/styles/main.css';
 
 ReactDOM.render(
   <div>
-    <CounterContainer />
     <AddItemContainer />
   </div>, document.getElementById("render-app")
 );
